8f5e35c49c8b4a3ee003f5e6b15ac6425970bd5f
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2025-04-11 22:41:05 +0530

    Added Routes with Versioning

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
-
+import v1Routes from "./routes/v1/v1Routes";
+import { config } from "./config/server.config";
 /**
  * DONOT DELETE THE BELOW app INSTANCE CREATION LINE i.e. const app = express();
  */
 const app = express();
 
+app.use(express.json());
+
+app.use(`/api/${config.API_VERSION}`, v1Routes); // ensure `/api/v1`
 
 /**
  * DONOT DELETE THE BELOW EXPORT STATEMENT
